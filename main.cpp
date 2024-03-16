e2acf1dcf1429e13baff23cf470fe7cf5a63b973
JACFIICayetano <159854188+JACFIICayetano@users.noreply.github.com>
2024-03-16 10:57:17 -0600

    Corto No.2 Jose Cayetano 0909-23-322

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    /*void ();{
+   int num1, num2, num3, mayor, medio, menor;
+
+   cout << "Ingrese el primer numero" << endl;
+   cin >> num1;
+   cin >> num2;
+   cin >> num3;
+
+   if ((num1 <= num2) && (num1 < num3)){
+       menor = num1;
+       if ( num2 <= num3){
+           medio = num2;
+           mayor =  num3;
+       } else {
+           medio = num3;
+           mayor = num2;
+       }
+   } else if ((num2 <= num1 ) && (num2 <  num3 )){
+       menor = num2;
+       if (num1 <= num3){
+           medio = num1;
+           mayor = num3;
+       } else {
+           medio = num3;
+           mayor = num1;
+       }
+   } else{
+       menor = num3;
+       if ( num1 <= num2){
+           medio = num1;
+           mayor = num2;
+       } else{
+           medio = num2;
+           mayor = num1;
+       }
+   }
+
+   cout << menor << endl;
+   cout << medio << endl;
+   cout << mayor << endl;
+
+*/
+
+    void ();{
+        int registro;
+        int numeros [10];
+        cout << "Ingresa 5 numeros del 1 al 10  para saber si esta en el vector" << endl;
+        cin >> registro;
+
+
+
+    }
+
+
+
+}
+
+
+
